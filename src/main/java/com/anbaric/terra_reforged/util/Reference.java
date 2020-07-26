a39a8033089e9a168d4f0cdd67bfb253621b2db7
AnbaricDragon <rymanweiler@hotmail.com>
2020-07-26 08:34:35 -0700

    Removal

--- src/main/java/com/anbaric/terra_reforged/util/Reference.java
+++
@@ -1,7 +0,0 @@
-package com.anbaric.terra_reforged.util;
-
-public class Reference
-{
-    public static final String MODID = "terra_reforged";
-    public static final String NAME = "Terra Reforged";
-}
--- src/main/java/com/anbaric/terra_reforged/util/interfaces/IBlockQuery.java
+++
@@ -1,9 +0,0 @@
-package com.anbaric.terra_reforged.util.interfaces;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorld;
-
-public interface IBlockQuery
-{
-    boolean matches(IWorld world, BlockPos pos);
-}
